727b4f7745420daa833eae410f1ac4a93e76842b
pratikdeore <58384739+pratikdeore@users.noreply.github.com>
2020-07-29 13:36:28 +0530

    website on devotional
    some info about lords

---
+++ index.js
@@ -0,0 +1,30 @@
+function brahma_chng1() {
+    document.getElementById("bd").src = "imgs/brahma1.jpg";
+}
+
+function brahma_chng2() {
+    document.getElementById("bd").src = "imgs/brahmadev2.jpg";
+}
+
+function vishnu_chng1() {
+    document.getElementById("nr").src = "imgs/narayan2.jpg";
+}
+
+function vishnu_chng2() {
+    document.getElementById("nr").src = "imgs/vishnu.jpg";
+}
+
+function mahadev_chng1() {
+    document.getElementById("md").src = "imgs/mahadev2.png";
+}
+
+function mahadev_chng2() {
+    document.getElementById("md").src = "imgs/mahadev.png";
+}
+
+function list()
+{
+    let list = document.getElementById("home");
+    list= document.createElement('li');
+    list = list.innerText="go back";
+}
